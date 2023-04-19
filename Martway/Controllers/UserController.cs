a3f72a48c9227b7c1d3b02f24d1999fc3a243185
Bruna Stephane da Silva <73487831+Brunastephane@users.noreply.github.com>
2023-04-19 14:42:32 +0100

    Add project files.

---
+++ Martway/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Martway.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Martway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("RetornaUser")]
+        public User RetornaUser()
+        {
+            var user = new User()
+            {
+                Name= "Test",
+                Email= "test@test.com",
+                Password="123456",
+                Id= 1
+            };
+
+
+            return user;
+        }
+
+        [HttpPut("AtualizaPassword")]
+
+        public User AtualizaPassword([FromBody] string newPassword)
+        {
+            var user = new User()
+            {
+                Name = "Test",
+                Email = "test@test.com",
+                Password = "123456",
+                Id = 1
+            };
+
+            user.Password = newPassword;
+
+
+            return user;
+        }
+
+    }
+}
---
+++ Martway/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Martway.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Martway.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
